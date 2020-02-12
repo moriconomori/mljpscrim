847867116bcad646f5f27164f128686c4d987bb4
Morico <moriconomori@gmail.com>
2020-02-12 21:42:50 +0900

    ページ遷移時のスクロール位置を設定

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,13 @@ const routes = [
 
 const router = new VueRouter({
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },  
 });
 
 export default router;
