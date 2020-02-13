1d5e9f8f7246b07b0854c60c21c6aa59dee51f8c
Morico <moriconomori@gmail.com>
2020-02-14 00:50:34 +0900

    Revert "Add Google analytics"
    This reverts commit caff0ed5daef13a36c5b7be9e2ed6090d5ede695.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
@@ -11,8 +10,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount("#app");
-
-Vue.use(VueAnalytics, {
-  id: "UA-158373345-1",
-  router,
-})
