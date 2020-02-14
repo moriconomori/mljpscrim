341647af0fc903a398f3f38954a123da92fbb0e5
Morico <moriconomori@gmail.com>
2020-02-14 19:20:09 +0900

    Add vue-gtag

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag"
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "UA-158373345-1" },
+}, router);
 
 new Vue({
   router,
