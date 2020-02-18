7245fbab416819246308206a975a644adc676947
Morico <moriconomori@gmail.com>
2020-02-19 02:54:59 +0900

    Get team.json

--- modules/api-build-json.js
+++ modules/api-build-json.js
@@ -2,11 +2,24 @@ import axios from 'axios'
 import fse from 'fs-extra'
 
 export default async function() {
-  const outputFile = './assets/json/teams.json'
+  const outputFileDir = './assets/json/'
+  const data = [
+    {
+      fileName: 'teams.json',
+      url:
+        'https://script.google.com/macros/s/AKfycbxtkRN9bqBbUyUj43B7epmVTkMz0LT5JK2Jx6qjxNuux2FZV4uu/exec',
+    },
+    {
+      fileName: 'team.json',
+      url:
+        'https://script.google.com/macros/s/AKfycbxlbEeWkJtQJaEIbKuuudYHcSXv_MmKSDK1zewcmNU_gwKyg5Y/exec',
+    },
+  ]
 
-  const res = await axios.get(
-    'https://script.google.com/macros/s/AKfycbxtkRN9bqBbUyUj43B7epmVTkMz0LT5JK2Jx6qjxNuux2FZV4uu/exec'
-  )
+  for (let i = 0; i < data.length; i++) {
+    const res = await axios.get(data[i].url)
 
-  fse.outputFile(outputFile, JSON.stringify(res.data))
+    const filePath = outputFileDir + data[i].fileName
+    fse.outputFile(filePath, JSON.stringify(res.data))
+  }
 }
