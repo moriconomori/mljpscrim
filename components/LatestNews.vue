4d52f9e59a522e2fce5ff4f4606d8b1c1990de0d
Morico <moriconomori@gmail.com>
2020-03-04 21:48:58 +0900

    news_latest.json を読む込むようにした

--- components/LatestNews.vue
+++ components/LatestNews.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="recent-news">
-    <div class="d-flex justify-space-between align-center">
+  <div class="latest-news">
+    <div class="d-flex justify-space-between align-center mb-n2">
       <section-header>
         お知らせ
       </section-header>
@@ -17,63 +17,65 @@
       </v-btn>
     </div>
 
-    <div class="d-flex flex-column">
-      <v-card v-for="news in newsList" :key="news.id" outlined tile>
-        <div class="d-flex justify-space-between">
-          <div class="subtitle-1 text-truncate pt-2 pl-4">
-            {{ news.title }}
-          </div>
-          <div class="caption text--secondary pt-2 px-4">
-            {{ news.date }}
-          </div>
-        </div>
-        <v-card-text class="news--text pt-0 mb-2">
-          {{ news.text }}
-        </v-card-text>
-      </v-card>
-    </div>
+    <v-card
+      v-for="(article, index) in news"
+      :key="index"
+      outlined
+      class="article ma-2 pb-4"
+    >
+      <v-row no-gutters>
+        <v-col cols="8">
+          <v-card-title class="pr-0">
+            <span class="text-truncate">{{ article.title }}</span>
+          </v-card-title>
+        </v-col>
+        <v-col cols="4">
+          <v-card-subtitle class="pl-0 text-right">
+            <v-icon size="14" class="text--secondary">
+              mdi-clock-outline
+            </v-icon>
+            {{ article.publishedAt }}
+          </v-card-subtitle>
+        </v-col>
+      </v-row>
+      <v-card-text class="article__body py-0">{{ article.body }}</v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
+import latestNewsJson from '~/assets/json/news_latest.json'
+
 export default {
   data: () => ({
-    newsList: [
-      {
-        id: 0,
-        title: '試合結果',
-        date: '2020.2.13',
-        text: 'Team A VS Team B の試合結果をお知らせします。',
-      },
-      {
-        id: 1,
-        title: '長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル',
-        date: '2020.2.12',
-        text: 'Team C がスクリムに参加しました。',
-      },
-      {
-        id: 2,
-        title: 'お知らせタイトル3',
-        date: '2020.2.11',
-        text:
-          '本文3行以上は省略本文3行以上は省略本文3行以上は省略本文3行以上は省略本文3行以上は省略本文3行以上は省略本文3行以上は省略',
-      },
-    ],
+    news: {
+      type: Array,
+    },
   }),
+
+  async created() {
+    if (latestNewsJson) {
+      this.news = latestNewsJson
+    } else {
+      const news = await this.$axios.$get(
+        'https://script.google.com/macros/s/AKfycbwmFnoU79NeiTjFWjrB_myQvTEZUohJrlD9S6TTSAGROq0qLqM/exec'
+      )
+      this.news = news
+    }
+  },
 }
 </script>
 
-<style>
-.recent-news .v-card:not(:last-child) {
-  border-bottom-style: none;
-}
-
-.news--text {
-  /* line-height: 1.375rem; */
-  height: 2.75rem;
+<style scoped>
+.article__body {
+  white-space: pre-line;
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 2;
   overflow: hidden;
+}
+
+.article .v-icon {
+  vertical-align: initial;
 }
 </style>
