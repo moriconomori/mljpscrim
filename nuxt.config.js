4182958a534106ab8aa5f50dc6b977987627b732
Morico <moriconomori@gmail.com>
2020-03-07 23:15:05 +0900

    Update title, description

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,15 +6,15 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | MLJP SCRIM',
+    title: 'MLJP SCRIM',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'MLJP スクリムの情報サイトです。',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
