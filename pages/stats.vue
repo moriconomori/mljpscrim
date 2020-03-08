1bed994df465b3d970ef8d635fc6f0b051ddd40f
Morico <moriconomori@gmail.com>
2020-03-08 18:09:30 +0900

    stats ページを追加

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <section>戦績</section>
+  <div class="stats">
+    <v-card v-if="!stats.length" outlined class="text-center ma-2 pa-8">
+      データがありません
+    </v-card>
+
+    <scrim-result-summary-card
+      v-for="(result, index) in stats"
+      :key="index"
+      :result="result"
+      class="ma-2"
+    />
   </div>
 </template>
 
 <script>
+import ScrimResultSummaryCard from '~/components/ScrimResultSummaryCard'
+import stats from '~/assets/data/stats.json'
+
 export default {
+  components: {
+    ScrimResultSummaryCard,
+  },
+
+  data: () => ({
+    stats,
+  }),
+
   head() {
     return {
       title: 'Stats',
