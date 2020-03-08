9d322f6dfa6a064fc5e83c3fe71d83d2ff30ff7f
Morico <moriconomori@gmail.com>
2020-03-08 18:52:06 +0900

    news ページを追加

---
+++ components/NewsArticleSummaryCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card outlined class="article pb-4">
+    <v-card-title class="d-block text-truncate">{{
+      article.title
+    }}</v-card-title>
+    <v-card-subtitle>
+      <v-icon size="14" class="text--secondary">mdi-clock-outline</v-icon>
+      {{ article.publishedAt }}</v-card-subtitle
+    >
+    <v-card-text class="pb-0">{{ article.body }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.v-card__text {
+  white-space: pre-line;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+}
+
+.article .v-icon {
+  vertical-align: initial;
+}
+</style>
--- pages/news.vue
+++ pages/news.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <section>お知らせ</section>
+  <div class="news">
+    <v-card v-if="!news.length" outlined class="text-center ma-2 pa-8">
+      データがありません
+    </v-card>
+
+    <news-article-summary-card
+      v-for="(article, index) in news"
+      :key="index"
+      :article="article"
+      class="ma-2"
+    />
   </div>
 </template>
 
 <script>
+import NewsArticleSummaryCard from '~/components/NewsArticleSummaryCard'
+import news from '~/assets/data/news.json'
+
 export default {
+  components: {
+    NewsArticleSummaryCard,
+  },
+
+  data: () => ({
+    news,
+  }),
+
   head() {
     return {
       title: 'News',
