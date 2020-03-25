57ed58f4b67f5ec9318b5217ab24bfb049af8c49
Morico <moriconomori@gmail.com>
2020-03-26 02:31:02 +0900

    Bottom navigation を作成

---
+++ src/components/TheBottomNavigation.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Icon from '@mdi/react';
+import { mdiHome, mdiAccountGroup, mdiTrophy, mdiNewspaper } from '@mdi/js';
+import Router, { useRouter } from 'next/router';
+
+const TheBottomNavigation = () => {
+  const router = useRouter();
+
+  const useStyles = makeStyles((theme) => ({
+    bottomNavi: {
+      top: 'auto',
+      bottom: 0,
+      '& .MuiTab-root': {
+        minHeight: 56,
+        padding: 0,
+        lineHeight: '1em',
+      },
+      '& .MuiTab-labelIcon .MuiTab-wrapper > *:first-child': {
+        marginBottom: 0,
+      },
+      '& .MuiTabs-indicator': {
+        display: 'none',
+      },
+      '& .MuiTab-root svg path': {
+        fill: theme.palette.text.secondary,
+      },
+      '& .MuiTab-root.Mui-selected svg path': {
+        fill: theme.palette.primary.main,
+      },
+    },
+  }));
+
+  const classes = useStyles();
+  const [currentPathName, setCurrentPathName] = useState(router.pathname);
+
+  useEffect(() => {
+    const handleRouteChange = (path) => {
+      setCurrentPathName(path);
+    };
+
+    Router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, []);
+
+  const handleChange = (event, newPath) => {
+    setCurrentPathName(newPath);
+  };
+
+  return (
+    <AppBar
+      position="fixed"
+      color="default"
+      className={classes.bottomNavi}
+      component="div"
+    >
+      <Tabs
+        value={currentPathName}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="primary"
+        textColor="primary"
+        aria-label="bottom navigation"
+      >
+        <Tab
+          icon={<Icon path={mdiHome} size={1} />}
+          label="HOME"
+          value="/"
+          onClick={() => Router.push('/')}
+        />
+        <Tab
+          icon={<Icon path={mdiAccountGroup} size={1} />}
+          label="クラン"
+          value="/teams"
+          onClick={() => Router.push('/teams')}
+        />
+        <Tab
+          icon={<Icon path={mdiTrophy} size={1} />}
+          label="戦績"
+          value="/stats"
+          onClick={() => Router.push('/stats')}
+        />
+        <Tab
+          icon={<Icon path={mdiNewspaper} size={1} />}
+          label="お知らせ"
+          value="/news"
+          onClick={() => Router.push('/news')}
+        />
+      </Tabs>
+    </AppBar>
+  );
+};
+
+export default TheBottomNavigation;
--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TheBottomNavigation from '../components/TheBottomNavigation';
 
 const Default = ({ children }) => {
+  const useStyles = makeStyles({
+    footer: {
+      paddingBottom: 56,
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <React.Fragment>
-      <header>header</header>
+      <AppBar position="static" color="default">
+        <Toolbar>MLJP SCRIM</Toolbar>
+      </AppBar>
       <main>{children}</main>
-      <footer>footer</footer>
+      <footer className={classes.footer}>footer</footer>
+      <TheBottomNavigation />
     </React.Fragment>
   );
 };
