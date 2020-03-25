c8731e8746a87469220ebbc8a13d74274b766b76
Morico <moriconomori@gmail.com>
2020-03-26 02:31:36 +0900

    theme カラーを変更

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,8 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import { amber } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: {
+      main: amber[500],
+    },
   },
 });
 
