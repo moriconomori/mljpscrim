b5daacd35b3e0d534ca36e8aad068b6e5c46a869
Morico <moriconomori@gmail.com>
2020-03-27 20:42:42 +0900

    propTypes を追加

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
@@ -38,6 +39,11 @@ const FooterLink = ({ href, text }) => {
       </Typography>
     </Link>
   );
+};
+
+FooterLink.propTypes = {
+  href: PropTypes.string,
+  text: PropTypes.string,
 };
 
 const TheFooter = () => {
